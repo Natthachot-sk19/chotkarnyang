82f5d025b1cdc1eb3ed706599c38e7770b47d6e6
natthachot <natthachot.ntc19@gmail.com>
2024-12-05 16:26:23 +0700

    fix link to a

--- src/Components/Social/Social.jsx
+++ src/Components/Social/Social.jsx
@@ -1,31 +1,46 @@
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTiktok, faLine } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faTiktok,
+  faLine,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Social = () => {
   return (
     <>
       <ul className="h-auto fixed top-[50%] right-4 bg-blue-500 text-white px-4 py-3 rounded-3xl shadow-md z-50 flex flex-col gap-5">
         <li>
-          <Link to={"//www.facebook.com"} target="_blank">
+          <a
+            href={"//www.facebook.com"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button className="text-white text-2xl hover:scale-110 duration">
               <FontAwesomeIcon icon={faFacebook} />
             </button>
-          </Link>
+          </a>
         </li>
         <li>
-          <Link to={"//www.tiktok.com"} target="_blank">
+          <a
+            href={"//www.tiktok.com"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button className="text-white text-2xl hover:scale-110 duration">
               <FontAwesomeIcon icon={faTiktok} />
             </button>
-          </Link>
+          </a>
         </li>
         <li>
-          <Link to={"https://line.me/ti/p/chFSPht1C4"} target="_blank">
+          <a
+            href={"https://line.me/ti/p/chFSPht1C4"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <button className="text-white text-2xl hover:scale-110 duration">
               <FontAwesomeIcon icon={faLine} />
             </button>
-          </Link>
+          </a>
         </li>
       </ul>
     </>
